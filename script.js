82ec813ec733a44a944402cfc7b30a65b6bb0cb2
leonardomartins578@gmail.com <leonardomartins578@gmail.com>
2022-10-19 10:37:09 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newtask input").value.length == 0) {
-    alert("Please Enter a Task");
+    alert("Por favor, adicione uma tarefa");
   } else {
     document.querySelector("#tasks").innerHTML += `
             <div class="task">
@@ -12,20 +12,21 @@ document.querySelector("#push").onclick = function () {
                 </button>
             </div>
         `;
+
     var current_tasks = document.querySelectorAll(".delete");
     for (var i = 0; i < current_tasks.length; i++) {
       current_tasks[i].onclick = function () {
         this.parentNode.remove();
       };
     }
+
     var tasks = document.querySelectorAll(".task");
     for (var i = 0; i < tasks.length; i++) {
       tasks[i].onclick = function () {
         this.classList.toggle("completed");
       };
     }
-  }
 
-  document.querySelector("#newtask input");
-  value = "";
+    document.querySelector("#newtask input").value = "";
+  }
 };
